1b87e9d411f226b7492497cebd3d38db35861c54
Paul Jensen <eastendmumbles@gmail.com>
2018-09-18 12:05:40 -0700

    modify compile.sh to remove non used dependency

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-g++ -o test scraper.cpp -std=c++11 -lcurl -lboost_system -DBOOST_NO_CXX11_SCOPED_ENUMS -lboost_filesystem -lstdc++fs -lboost_iostreams -lz -g -ljsoncpp
+g++ -o test scraper.cpp -std=c++11 -lcurl -lboost_system -DBOOST_NO_CXX11_SCOPED_ENUMS -lboost_filesystem -lboost_iostreams -lz -ljsoncpp
 
