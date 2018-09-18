9c6073a11f478d159580e1bccb20ed4723c62729
Paul Jensen <eastendmumbles@gmail.com>
2018-09-18 14:56:23 -0700

    compile and startup

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
-g++ -o test scraper.cpp -std=c++11 -lcurl -lboost_system -DBOOST_NO_CXX11_SCOPED_ENUMS -lboost_filesystem -lboost_iostreams -lz -ljsoncpp
+g++ -o gridcoinscraper scraper.cpp -std=c++11 -lcurl -lboost_system -DBOOST_NO_CXX11_SCOPED_ENUMS -lboost_filesystem -lboost_iostreams -lz -ljsoncpp
 
